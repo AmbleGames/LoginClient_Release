b200877644a6d476ddd97e24d48cc88247dcda99
AmbleGames <betaz@online.de>
2015-03-01 19:24:26 +0100

    Source and Release Upload
    I've uploaded a Release with which you can test whether it works. And I
    uploaded a Source Code for Visual Studio

---
+++ Source/LoginClient/Success.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace BetaLoginClient_MySQL_
+{
+    public partial class Success : Form
+    {
+        public Success()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
